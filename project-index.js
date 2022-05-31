4e4555908598332514622342d40b9eb10df34e6c
UniBreakfast <unibreakfast1@gmail.com>
2022-05-31 23:41:57 +0300

    make projectIndex

--- project-index.js
+++ project-index.js
@@ -9,29 +9,30 @@ Object.assign(projectIndex, getObj('./index.js', {
   exports: {},
   exportsTo: [],
   requires: {
-    main: require('./main'),
+    main: require('./main.js'),
+    projectIndex: require('./project-index.js'),
   },
   requiresFrom: [
     getObj('./main.js', {
       path: './main.js',
       fileName: 'main.js',
       exports: {
-        main: require('./main'),
+        main: require('./main.js'),
       },
       exportsTo: [
         getObj('./index.js'),
       ],
       requires: {
-        stayRunning: require('./stay-running'),
-        getRootItemsList: require('./get-root-items-list'),
-        listTopLevelFunctionsInAFile: require('./list-top-level-funcs-in-file'),
+        stayRunning: require('./stay-running.js'),
+        getRootItemsList: require('./get-root-items-list.js'),
+        listTopLevelFunctionsInAFile: require('./list-top-level-funcs-in-file.js'),
       },
       requiresFrom: [
         getObj('./stay-running.js', {
           path: './stay-running.js',
           fileName: 'stay-running.js',
           exports: {
-            stayRunning: require('./stay-running'),
+            stayRunning: require('./stay-running.js'),
           },
           exportsTo: [
             getObj('./main.js'),
@@ -43,7 +44,7 @@ Object.assign(projectIndex, getObj('./index.js', {
           path: './get-root-items-list.js',
           fileName: 'get-root-items-list.js',
           exports: {
-            getRootItemsList: require('./get-root-items-list'),
+            getRootItemsList: require('./get-root-items-list.js'),
           },
           exportsTo: [
             getObj('./main.js'),
@@ -70,7 +71,7 @@ Object.assign(projectIndex, getObj('./index.js', {
           path: './list-top-level-funcs-in-file.js',
           fileName: 'list-top-level-funcs-in-file.js',
           exports: {
-            listTopLevelFunctionsInAFile: require('./list-top-level-funcs-in-file'),
+            listTopLevelFunctionsInAFile: require('./list-top-level-funcs-in-file.js'),
           },
           exportsTo: [
             getObj('./main.js'),
@@ -105,6 +106,63 @@ Object.assign(projectIndex, getObj('./index.js', {
               ],
               requires: {},
               requiresFrom: [],
+            }),
+          ],
+        }),
+      ],
+    }),
+    getObj('./project-index.js', {
+      path: './project-index.js',
+      fileName: 'project-index.js',
+      exports: {
+        projectIndex: require('./project-index.js'),
+      },
+      exportsTo: [
+        getObj('./index.js'),
+      ],
+      requires: {
+        getObj: require('./get-obj.js'),
+      },
+      requiresFrom: [
+        getObj('./get-obj.js', {
+          path: './get-obj.js',
+          fileName: 'get-obj.js',
+          exports: {
+            getObj: require('./get-obj.js'),
+          },
+          exportsTo: [
+            getObj('./project-index.js'),
+          ],
+          requires: {
+            merge: require('./merge.js'),
+          },
+          requiresFrom: [
+            getObj('./merge.js', {
+              path: './merge.js',
+              fileName: 'merge.js',
+              exports: {
+                merge: require('./merge.js'),
+              },
+              exportsTo: [
+                getObj('./get-obj.js'),
+              ],
+              requires: {
+                makeMerge: require('./make-merge.js'),
+              },
+              requiresFrom: [
+                getObj('./make-merge.js', {
+                  path: './make-merge.js',
+                  fileName: 'make-merge.js',
+                  exports: {
+                    makeMerge: require('./make-merge.js'),
+                  },
+                  exportsTo: [
+                    getObj('./merge.js'),
+                  ],
+                  requires: {},
+                  requiresFrom: [],
+                }),
+              ],
             }),
           ],
         }),
